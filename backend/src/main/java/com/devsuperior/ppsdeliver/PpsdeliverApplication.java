023a8315b5004f4fd10260fd2721ae96426b86cf
Pedro Paulo dos Santos <pedro2206@live.com>
2021-01-09 06:02:34 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/ppsdeliver/PpsdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ppsdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpsdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpsdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/ppsdeliver/PpsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ppsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
